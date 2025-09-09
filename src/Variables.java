3dfe6f1fa66a270c6624afba8670092acb9dd79e
Uday531 <adepuuday51@gmail.com>
2025-09-09 23:38:33 +0530

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a=1;
+        double b=2.0;
+        char c='A';
+        String s="Uday";
+        boolean t=true;
+        System.out.println(a+" "+b+" "+c+" "+s+" "+t);
+    }
+}
